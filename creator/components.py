11bbc9bfe6a2b143cf21931a0507dc311ba3d2b9
dhung1 <davidhung@email.arizona.edu>
2017-01-18 16:53:00 -0700

    Initital commit of working code. Generated 29251 targets (not included)

--- creator/components.py
+++ creator/components.py
@@ -2,46 +2,54 @@
 # Author: dhung
 # Description: This file holds the building blocks for artificial target creation 
 
-# List of background images - TODO
-background = ["../res/backgrounds/",
+# List of background images - TODO: desert, cement/asphalt (runway)
+backgrounds = [("grass", "../res/backgrounds/grass.jpg"),]
 
-              "../res/backgrounds/"]
-
-# List of shapes - TODO
-shape = ["../res/shapes/",
-        
-         "../res/shapes/"]
+# List of shapes
+shapes = [("circle", "../res/shapes/circle.jpg"),
+         ("cross", "../res/shapes/cross.jpg"),
+         ("heptagon", "../res/shapes/heptagon.jpg"),
+         ("hexagon", "../res/shapes/hexagon.jpg"),
+         ("octagon", "../res/shapes/octagon.jpg"),
+         ("pentagon", "../res/shapes/pentagon.jpg"),
+         ("quartercircle", "../res/shapes/quartercircle.jpg"),
+         ("rectangle", "../res/shapes/rectangle.jpg"),
+         ("semicircle", "../res/shapes/semicircle.jpg"),
+         ("square", "../res/shapes/square.jpg"),
+         ("star", "../res/shapes/star.jpg"),
+         ("trapezoid", "../res/shapes/trapezoid.jpg"),
+         ("triangle", "../res/shapes/triangle.jpg"),]
 
 # List of letters
-letter = ["../res/letters/A.png",
-          "../res/letters/B.png",
-          "../res/letters/C.png",
-          "../res/letters/D.png",
-          "../res/letters/E.png",
-          "../res/letters/F.png",
-          "../res/letters/G.png",
-          "../res/letters/H.png",
-          "../res/letters/I.png",
-# TODO    "../res/letters/J.png",
-          "../res/letters/K.png",
-          "../res/letters/L.png",
-          "../res/letters/M.png",
-          "../res/letters/N.png",
-          "../res/letters/O.png",
-          "../res/letters/P.png",
-# TODO    "../res/letters/Q.png",
-          "../res/letters/R.png",
-          "../res/letters/S.png",
-          "../res/letters/T.png",
-          "../res/letters/U.png",
-          "../res/letters/V.png",
-          "../res/letters/W.png",
-          "../res/letters/X.png",
-          "../res/letters/Y.png",
-          "../res/letters/Z.png"]
+letters = [("A", "../res/letters/A.jpg"),
+          ("A", "../res/letters/B.jpg"),
+          ("B", "../res/letters/C.jpg"),
+          ("C", "../res/letters/D.jpg"),
+          ("D", "../res/letters/E.jpg"),
+          ("E", "../res/letters/F.jpg"),
+          ("F", "../res/letters/G.jpg"),
+          ("G", "../res/letters/H.jpg"),
+          ("H", "../res/letters/I.jpg"),
+          ("I", "../res/letters/J.jpg"),
+          ("K", "../res/letters/K.jpg"),
+          ("L", "../res/letters/L.jpg"),
+          ("M", "../res/letters/M.jpg"),
+          ("N", "../res/letters/N.jpg"),
+          ("O", "../res/letters/O.jpg"),
+          ("P", "../res/letters/P.jpg"),
+          ("Q", "../res/letters/Q.jpg"),
+          ("R", "../res/letters/R.jpg"),
+          ("S", "../res/letters/S.jpg"),
+          ("T", "../res/letters/T.jpg"),
+          ("U", "../res/letters/U.jpg"),
+          ("V", "../res/letters/V.jpg"),
+          ("W", "../res/letters/W.jpg"),
+          ("X", "../res/letters/X.jpg"),
+          ("Y", "../res/letters/Y.jpg"),
+          ("Z", "../res/letters/Z.jpg")]
 
 # List of colors in GBR (OpenCV compatibility)
-color = [('white',  (255,255,255)),
+colors = [('white',  (255,255,255)),
          ('black',  (0,0,0)),
          ('gray',   (160,160,160)),
          ('red',    (0,0,255)),
--- creator/create_targets.py
+++ creator/create_targets.py
@@ -1,16 +1,52 @@
 import cv2
 import numpy as np
+import os
 
 from components import *
 
-# Temporary test code
-img = cv2.imread(letter[0])
+DEBUG = False
 
-# The two lines below are only necessary for an interactive python session
-# cv2.startWindowThread()
-# cv2.namedWindow("Image")
-
-cv2.imshow("Image", img)
-
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+# Iterate through backgrounds
+for background in backgrounds:
+    bg = cv2.imread(background[1])
+    # Iterate through shapes
+    for shape in shapes:
+        # Read shape in. Note current iteration has shape as black on white
+        targetShape = cv2.imread(shape[1])
+        targetShape_gray = cv2.cvtColor(targetShape, cv2.COLOR_BGR2GRAY)
+        ret, targetShape_mask = cv2.threshold(targetShape_gray, 10, 255, cv2.THRESH_BINARY)
+        # Iterate through letters
+        for letter in letters:
+            # Read letter in. Note current iteration has letter as white on black
+            targetLetter = cv2.imread(letter[1])
+            targetLetter_gray = cv2.cvtColor(targetLetter, cv2.COLOR_BGR2GRAY)
+            ret, targetLetter_mask = cv2.threshold(targetLetter_gray, 10, 255, cv2.THRESH_BINARY)
+            assert targetLetter.shape == targetShape.shape
+            # Iterate through target colors
+            for color in colors:
+                targetColor = color[1]
+                # Iterate through letter colors
+                for letterColor in colors:
+                    # Check for same colors, skip (assume will never have)
+                    if letterColor == color:
+                        continue
+                    targetLetterColor = letterColor[1]
+                    # Grab random slice of background - TODO
+                    targetBg = bg[0:targetShape.shape[0], 0:targetShape.shape[1]]
+                    # Construct target
+                    target_top = cv2.bitwise_and(cv2.bitwise_not(targetLetter), targetLetterColor, mask=cv2.bitwise_not(targetLetter_mask))
+                    target_mid = cv2.bitwise_and(cv2.bitwise_not(targetShape), targetColor, mask=targetLetter_mask)
+                    target_bot = cv2.bitwise_and(targetBg, targetBg, mask=targetShape_mask)
+                    target = cv2.bitwise_or(target_top, target_mid)
+                    target = cv2.bitwise_or(target, target_bot)
+                    
+                    # Save target
+                    directory = '../generated/'
+                    if not (os.path.isdir(directory)):
+                        os.mkdir(directory)
+                    filename = background[0] + '_' + color[0] + '_' + shape[0] + '_' + letterColor[0] + '_' + letter[0] + '.jpg'
+                    cv2.imwrite(directory + filename, target)
+                    if DEBUG:
+                        cv2.imshow("Debug", target)
+                        cv2.waitKey(0)
+                        cv2.destroyAllWindows()
